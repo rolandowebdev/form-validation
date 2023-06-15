f6b1afdfc5d6cb17dd8a5e6341f9d700a09dc8b3
RolandoPranataAR <rolandopranata@gmail.com>
2023-06-15 09:17:05 +0700

    chore: change any type for ErrorMessageProps into specific type

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,9 +1,9 @@
-import { Box, Typography } from '@mui/material'
 import { ErrorOutline } from '@mui/icons-material'
+import { Box, Typography } from '@mui/material'
+import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form'
 
 type ErrorMessageProps = {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	message: any
+	message: string | FieldError | Merge<FieldError, FieldErrorsImpl> | undefined
 }
 
 export const ErrorMessage: React.FC<ErrorMessageProps> = ({
@@ -13,7 +13,7 @@ export const ErrorMessage: React.FC<ErrorMessageProps> = ({
 		<Box sx={{ display: 'flex', alignItems: 'center', gap: '5px', mt: '6px' }}>
 			<ErrorOutline color='error' fontSize='small' />
 			<Typography color='error.main' variant='body1' fontSize={14}>
-				{message}
+				<>{message}</>
 			</Typography>
 		</Box>
 	)
